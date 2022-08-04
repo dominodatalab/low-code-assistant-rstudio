6810e8e6e649736199c47811c63bf6a2a0d3e3ae
Dean Attali <daattali@gmail.com>
2022-08-04 13:54:04 -0400

    move datasettransformation to separate file

--- R/transformation.R
+++ R/transformation.R
@@ -103,24 +103,4 @@ FilterTransformation$TRANSFORMATIONS <- list(
 
 
 
-DatasetTransformation <- R6::R6Class(
-  "DatasetTransformation",
 
-  public = list(
-
-    data = NULL,
-    transformations = NULL,  # TransformationSequence object
-
-    initialize = function(data, transformations = NULL) {
-      self$data <- data
-      self$transformations <- transformations
-      invisible(self)
-    },
-
-    run = function(env = parent.frame()) {
-      assign(self$transformations$get_name_in(), self$data, envir = env)
-      self$transformations$run(env = env)
-    }
-
-  )
-)
