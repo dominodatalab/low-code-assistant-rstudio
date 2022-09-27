24f2b9ac587830d7936dcdb70f6b1416830ecee3
Dean Attali <daattali@gmail.com>
2022-09-27 19:53:56 -0400

    add functions assist_data() assist_transform() assist_viz() to launch the separate modules, or assist() to launch together

--- R/run_app.R
+++ R/run_app.R
@@ -11,11 +11,38 @@ app_server <- function(input, output, session) {
 }
 
 #' @export
-run_app <- function() {
+assist <- function() {
   shiny::shinyApp(app_ui, app_server, options = list(launch.browser = TRUE))
 }
 
 #' @export
 run_gadget <- function() {
   shiny::runGadget(app_ui, app_server, viewer = shiny::dialogViewer("Domino R Assistant", 1100, 800))
+}
+
+#' @export
+assist_data <- function() {
+  app <- shinyApp(
+    ui = fluidPage(page_data_select_ui("app", standalone = TRUE)),
+    server = function(input, output, session) page_data_select_server("app")
+  )
+  shiny::runGadget(app, viewer = shiny::dialogViewer("Domino R Assistant", 1100, 800))
+}
+
+#' @export
+assist_transform <- function() {
+  app <- shinyApp(
+    ui = fluidPage(page_xforms_ui("app", standalone = TRUE)),
+    server = function(input, output, session) page_xforms_server("app")
+  )
+  shiny::runGadget(app, viewer = shiny::dialogViewer("Domino R Assistant", 1100, 800))
+}
+
+#' @export
+assist_viz <- function() {
+  app <- shinyApp(
+    ui = fluidPage(page_viz_ui("app", standalone = TRUE)),
+    server = function(input, output, session) page_viz_server("app")
+  )
+  shiny::runGadget(app, viewer = shiny::dialogViewer("Domino R Assistant", 1100, 800))
 }
