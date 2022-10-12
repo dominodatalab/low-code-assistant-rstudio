aa1685fda5d145cdf80118dd4053bdde50a5e69b
Dean Attali <daattali@gmail.com>
2022-10-12 02:36:32 -0400

    define all mixpanel constants

---
+++ R/mixpanel.R
@@ -0,0 +1,10 @@
+MIXPANEL_TOKEN <- "c7c62b2aa8085f53d86952350a13b64a"
+MIXPANEL_CONFIG <- list("property_blacklist" = c('$current_url', '$referrer', '$initial_referrer'))
+MIXPANEL_TEST_TOKEN <- "3aef1937b53691cfad358cbe583c0aa6"
+MIXPANEL_TEST_DOMAINS <- c('localhost', '127.0.0.1', '0.0.0.0', '.dominodatalab.com', 'team-sandbox.domino.tech', 'eval.domino.tech', 'domino-eval.com', 'domino-pilot.com', 'dominohackathon.com', 'dominodata.science', 'gurudomino.com', 'dominoai1.com')
+MIXPANEL_EVENT_INIT <- "LCA open"
+MIXPANEL_EVENT_CODE <- "LCA inserted code"
+MIXPANEL_EVENT_INTERACTION <- "LCA interaction"
+MIXPANEL_SECTION_LOAD <- "load_data"
+MIXPANEL_SECTION_XFORM <- "transformations"
+MIXPANEL_SECTION_VIZ <- "visualizations"
