3ce81e71b25fccd23e045f00840435e182c1d8d0
Dean Attali <daattali@gmail.com>
2023-01-03 16:30:15 -0500

    snippets: fix a snippet that didn't have working code

--- inst/sample_snippets/snippets/machine learning/knn.R
+++ inst/sample_snippets/snippets/machine learning/knn.R
@@ -2,7 +2,7 @@
 train <- rbind(iris3[1:25,,1], iris3[1:25,,2], iris3[1:25,,3])
 test <- rbind(iris3[26:50,,1], iris3[26:50,,2], iris3[26:50,,3])
 cl <- factor(c(rep("s",25), rep("c",25), rep("v",25)))
-knn(train, test, cl, k = 3, prob=TRUE)
+class::knn(train, test, cl, k = 3, prob=TRUE)
 
 # Example 2
 i <- grep("Length", names(iris))
